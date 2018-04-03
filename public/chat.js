7df086660ae443e3de20b3b96c5bdbac8569b446
Tim Ryan <tryan@valcom.com>
2018-04-03 12:43:30 -0400

    Attach username to messages in chat window for identification.

--- public/chat.js
+++ public/chat.js
@@ -4,22 +4,29 @@ window.onload = () => {
   const field = document.getElementById('field');
   const sendButton = document.getElementById('send');
   const content = document.getElementById('content');
+  const name = document.getElementById('name');
 
   socket.on('message', (data) => {
     if (data.message) {
-      messages.push(data.message);
+      messages.push(data);
       let html = '';
       for (let i=0; i < messages.length; i++) {
-        html += messages[i] + '<br />';
+        html += '<b>' + (messages[i].username ? messages[i].username : 'anonymous') + ': </b>';
+        html += messages[i].message + '<br />';
       }
       content.innerHTML = html;
+      content.scrollTop = content.scrollHeight;
     } else {
       console.log(`There is a problem: ${data}`);
     }
   });
 
-  sendButton.onclick = () => {
-    let text = field.value;
-    socket.emit('send', { message: text });
+  sendButton.onclick = sendMessage = () => {
+    if (name.value == "") {
+      alert('Please type your name!');
+    } else {
+      let text = field.value;
+      socket.emit('send', { message: text, username: name.value });
+    }
   };
 }
